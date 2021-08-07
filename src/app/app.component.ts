acd1d26d399a9a5fd1ad85cd46aa5a2fd55e6ff1
Wai The Guy <waitheguy@gmail.com>
2021-08-07 16:00:55 -0700

    Adding Log In and Sign Up headers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,6 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { FcmService } from './services/fcm.service';
 import { Plugins, NetworkStatus, PluginListenerHandle } from '@capacitor/core';
 import { GoogleAnalytics } from '@ionic-native/google-analytics/ngx';
-<<<<<<< HEAD
-
-
-
-
 
 const { Network} = Plugins;
 
@@ -19,14 +14,13 @@ const { Network} = Plugins;
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss']
-})
+  })
 export class AppComponent {
 
   networkStatus: NetworkStatus;
   networkListener: PluginListenerHandle;
 
   constructor(
-    private analyticsService: AnalyticsService,
     private platform: Platform,
     private splashScreen: SplashScreen,
     private statusBar: StatusBar,
@@ -48,34 +42,34 @@ export class AppComponent {
           this._loader.dismiss();
         } else {
           this.presentLoading('You are offline. Waiting for internet connection.');
-        }
+          }
       });
       this.networkStatus = await Network.getStatus();
       // Trigger the push setup 
 
       this.fcmService.initPush();
-      this.ga.startTrackerWithId('G-280502510')
-      .then(() => {
-        console.log('Google analytics is ready now');
-        this.ga.trackView('Outbox') 
-        .then(() => {
+  //     this.ga.startTrackerWithId('G-280502510')
+  //     .then(() => {
+  //       console.log('Google analytics is ready now');
+  //       this.ga.trackView('Outbox') 
+  //       .then(() => {
 
-        })
-        .catch(
-          error => console.log(error)
-        );  
-       }).catch(
-        error => console.log('Google Analytics Error: ' + error)
-      );
+  //       })
+  //       .catch(
+  //         error => console.log(error)
+  //       );  
+  //      }).catch(
+  //       error => console.log('Google Analytics Error: ' + error)
+  //     );
       
-    });
-  }
+  //   });
+  // }
 
-  // Track an event:
-  trackEvent(val) {
-    // Label and Value are optional, Value is numeric
-    this.ga.trackEvent('Category', 'Action', 'Label', val)
-  }  
+  // // Track an event:
+  // trackEvent(val) {
+  //   // Label and Value are optional, Value is numeric
+  //   this.ga.trackEvent('Category', 'Action', 'Label', val)
+  // }  
 
     });
   }
