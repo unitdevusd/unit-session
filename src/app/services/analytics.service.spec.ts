51f2b7da343eda996890d291199bd0fc3c70f436
Wai The Guy <waitheguy@gmail.com>
2021-08-11 02:16:52 -0700

    editing analytics service spec file

--- src/app/services/analytics.service.spec.ts
+++ src/app/services/analytics.service.spec.ts
@@ -2,7 +2,7 @@ import { TestBed } from '@angular/core/testing';
 
 import { AnalyticsService } from './analytics.service';
 
-describe('Analytics.Service.TsService', () => {
+describe('AnalyticsService', () => {
   let service: AnalyticsService;
 
   beforeEach(() => {
